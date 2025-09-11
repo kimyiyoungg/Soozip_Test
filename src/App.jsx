a60f5de6cb92262b3a7f2f9984fb0a9f2358b4d1
‘kimyiyoungg’ <2eeeee0inog@gmail.com>
2025-09-11 21:36:48 +0900

    저장

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { Routes, Route, useNavigate } from "react-router-dom";
 import Home from './Home';
-import RoombtiTest from "./RoombtiTest";
+
 import RoomCounsult from './RoomConsult';
 import StylingTypePage from './StylingTypePage';
 import StylingInfoPage from './StylingInfoPage';
