88331e0bff40aa6ed79248e2126093210b6788ff
YERIN0804 <dpfls0570@gmail.com>
2025-11-19 22:49:22 +0900

    결과 로딩 화면

--- src/Home.jsx
+++ src/Home.jsx
@@ -8,7 +8,7 @@ export default function Home() {
       style={{
         width: 408,
         minHeight: 600,
-        height: "100dvh", //새로로 꽉차게 ,dvh는 주소창 높이를 제외한 실제 보이는 영역
+        height: "100dvh", // 세로 꽉 차게
         display: "flex",
         flexDirection: "column",
         alignItems: "center",
@@ -16,43 +16,65 @@ export default function Home() {
         background: "#fbf2d5",
         padding: "2rem 1rem",
         boxSizing: "border-box",
-        cursor: "default"
+        cursor: "default",
       }}
     >
-      <div style={{ textAlign: "center", cursor:"default"}}>
-        <p style={{ fontSize: 12, fontWeight: 600, color: "#fe6a0f" }}>
-          soozip
-        </p>
-        <p style={{ fontSize: 16, fontWeight: 600, color: "rgba(0,0,0,0.68)" }}>
+      {/* 상단 로고 + 텍스트 */}
+      <div style={{ textAlign: "center", cursor: "default" }}>
+        {/* 로고 + '수집 방BTI' 한 줄 배치 */}
+        <div
+          style={{
+            display: "flex",
+            alignItems: "center", // 세로 가운데 정렬
+            justifyContent: "center", // 전체 중앙 정렬
+            gap: "6px", // 로고와 글씨 사이 간격
+            margin: "30px 0 8px 0",
+          }}
+        >
+          <img
+            src="src/assets/soozip_logo.png"
+            alt="로고"
+            style={{
+              width: "20px", // 작게!
+              height: "20px",
+              objectFit: "contain",
+              position: "relative",
+              top: "40px",
+            }}
+          />
+          <p
+            style={{
+              fontSize: 25,
+              fontWeight: 600,
+              color: "rgba(0,0,0,0.68)",
+              margin: 0, // 기본 여백 제거
+              position: "relative",
+              top: "40px",
+            }}
+          >
+            수집 방BTI
+          </p>
+        </div>
+
+        <p style={{ fontSize: 45, fontWeight: 1000, color: "#000" }}>
           나의 집꾸 스타일은?
-        </p>
-        <p style={{ fontSize: 20, fontWeight: 600, color: "#000" }}>
-          내가 어떤 공간을 좋아하는지 알아보세요.
         </p>
       </div>
 
-      {/* <img
+      {/* 중앙 이미지 */}
+      <img
         src="src/assets/bear3.png"
         alt="main"
         style={{
-          width: 250,
-          height: 250,
-          objectFit: "cover",
-          margin: "2rem 0",
-        }}
-      /> */}
-
-      <img
-        src="src/assets/main_bear.png"
-        alt="main"
-        style={{
-          width: 250,
-          height: 250,
+          marginBottom: "50px",
+          width: 300,
+          height: 300,
           objectFit: "cover",
           margin: "2rem 0",
         }}
       />
 
+      {/* 하단 버튼 */}
       <button
         style={{
           width: "100%",
@@ -62,7 +84,7 @@ export default function Home() {
           background: "#000",
           border: "1px solid #ddd9d9",
           color: "#fff",
-          fontSize: 19,
+          fontSize: 25,
           fontWeight: 600,
           display: "flex",
           alignItems: "center",
@@ -71,7 +93,7 @@ export default function Home() {
         }}
         onClick={() => navigate("/RoombtiTest")}
       >
-        방BTI 시작하기
+        방BTI 알아보기
       </button>
     </div>
   );
